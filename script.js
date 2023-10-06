c813abc6b4da01773e850c65a985b81754ec4537
Tulasi Latha Gangadhari <65495688+TulasiLatha@users.noreply.github.com>
2023-10-06 21:19:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 //your JS code here. If requir
 document.addEventListener("DOMContentLoaded", () => {
-const button=document.getElementById("btn");
-
-const output=document.getElementById("output");
-const input=document.getElementById("ip");
+ const input = document.getElementById("ip");
+  const button = document.getElementById("btn");
+  const output = document.getElementById("output");
 
 	function createPromiseWithDelay(delay,callback){
 		return new Promise((resolve)=>{
@@ -16,7 +15,7 @@ button.addEventListener("click",()=>{
 	const inputValue=parseFloat(input.value);//converting into number
 
 	createPromiseWithDelay(2000, ()=>inputValue)//created promise
-		 .then((result) => createPromiseWithDelay(2000, () => result))
+		 .then((Result) => createPromiseWithDelay(2000, () => Result))
 	.then((Result)=>createPromiseWithDelay(1000, ()=>Result*2))
 	.then((Result)=>createPromiseWithDelay(1000, ()=>Result-3))
 	.then((Result)=>createPromiseWithDelay(1000,()=>Result/2))
