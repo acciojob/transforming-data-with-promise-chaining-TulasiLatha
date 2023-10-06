a2ba3cbba8ba014b9a68a6c3f1ee2d92905b374e
Tulasi Latha Gangadhari <65495688+TulasiLatha@users.noreply.github.com>
2023-10-06 20:41:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
-//your JS code here. If required.
+//your JS code here. If requir
+const button=document.getElementById("btn");
+
+const output=document.getElementById("output");
+const input=document.getElementByid("ip");
+
+	function createPromiseWithDelay(delay,callback){
+		return new Promise((resolve)=>{
+			setTimeOut(()=>{
+				resolve(callback());
+			},delay)
+		})
+	}
+button.addEventListener("click",()=>{
+	const inputValue=parseFloat(input.value);
+
+	createPromiseWithDelay(2000, ()=>inputValue)
+	.then((result)=>createPromiseWithDelay(1000, ()=>result*2))
+	.then((result)=>createPromiseWithDelay(1000, ()=>result-3))
+	.then((result)=>createPromiseWithDelay(1000,()=>result/2))
+	.then((result)=>createPromiseWithDelay(1000, ()=>result+2))
+	.then((finalResult)=>{
+		output.textContent=`finalresult:${finalResult}`;
+		
+	})
+	.catch((error)=>{
+		console.error(error);
+	})
+})
+	
